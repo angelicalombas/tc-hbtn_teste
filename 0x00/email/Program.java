1a7e2b5d65210add9a912033964af3a1f043cfcb
angelicalombas <angelica_lombas@hotmail.com>
2022-03-04 13:37:15 -0300

    exercicio 0

--- 0x00/email/Program.java
+++ 0x00/email/Program.java
@@ -2,11 +2,11 @@ public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Teste de email válido");
+        System.out.println("Teste de email valido");
         boolean isValidEmail = TesteEmail.testar_email_com_arroba();
         System.out.println(isValidEmail);
 
-        System.out.println("Teste de email sem @");
+        System.out.println("Teste de email sem arroba");
         isValidEmail = TesteEmail.testar_email_sem_arroba();
         System.out.println(isValidEmail);
 
--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -5,11 +5,11 @@ public class TesteEmail {
     }
 
     public static boolean testar_email_sem_arroba(){
-        return   Pessoa.emailValid("email_testedominio.com.br");
+        return   !Pessoa.emailValid("email_testedominio.com.br");
     }
 
     public static boolean testar_email_mais_50_caracteres(){
-        return   Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br");
+        return   !Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br");
     }
 
 }
