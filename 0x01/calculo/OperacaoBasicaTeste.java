e9fa8091ff3355af9f360f076192285b0839e03f
angelicalombas <angelica_lombas@hotmail.com>
2022-03-07 09:28:49 -0300

    exercicio 0

--- 0x01/calculo/OperacaoBasicaTeste.java
+++ 0x01/calculo/OperacaoBasicaTeste.java
@@ -29,31 +29,19 @@ public class OperacaoBasicaTeste {
     }
 
     public static boolean testar_sqrt() {
-        if (OperacaoBasica.sqrt(49) == 7) {
-            return true;
-        }
-        return false;
+        return OperacaoBasica.sqrt(49) == 7;
     }
 
     public static boolean testar_primo() {
-        if (OperacaoBasica.numero_primo(7).equals("Numero válido, o numero é primo")) {
-            return true;
-        }
-        return false;
+        return OperacaoBasica.numero_primo(7).equals("Numero válido, o numero é primo");
     }
 
     public static boolean testar_nao_primo() {
-        if (OperacaoBasica.numero_primo(4).equals("Número válido, mas o numero não é primo")) {
-            return false;
-        }
-        return true;
+        return !OperacaoBasica.numero_primo(4).equals("Número válido, mas o numero não é primo");
     }
 
     public static boolean testar_primo_menor_zero() {
-        if (OperacaoBasica.numero_primo(-2).equals("Número invalido. Numero menor ou igual a 1 não é primo")) {
-            return false;
-        }
-        return true;
+        return !OperacaoBasica.numero_primo(-2).equals("Número invalido. Numero menor ou igual a 1 não é primo");
     }
 
 }
